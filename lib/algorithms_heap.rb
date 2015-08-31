10f7a92e064c2cd7f1439c9b1e9e432ae1cb73a6
apradhn <abhijit.pradhan6@gmail.com>
2015-08-31 17:30:17 -0400

    completes lab

--- lib/algorithms_heap.rb
+++ lib/algorithms_heap.rb
@@ -1,1 +1,27 @@
-# code your solution here
+require "pry"
+class MinHeap
+  attr_accessor :data, :root, :subroot
+
+  def initialize
+    @data = []
+  end
+
+  def push(element)
+    if @data.empty?
+      @root = element
+    end
+    if element <= @root
+      @subroot = @root
+      @root = element
+    end
+    @data << element
+  end
+
+  def pop
+    if @data.last == @root && @subroot != @root
+      @root = @subroot
+    end
+    @data.pop
+  end
+
+end
